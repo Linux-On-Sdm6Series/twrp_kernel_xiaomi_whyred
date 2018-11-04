e260d0a3bf76ef41e452706c10381725d5c97e20
Vasishath Kaushal <vasishath@gmail.com>
2018-11-04 21:12:42 +0530

    Add pstore ramoops memory reservation

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -308,13 +308,9 @@ static void msm_restart_prepare(const char *cmd)
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-	} else {
-		if (force_warm_reset) {
-			qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-		} else {
-			qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
-		}
-	}
+	else
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+
 	if (in_panic) {
 			   qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
        } else if (cmd != NULL) {
